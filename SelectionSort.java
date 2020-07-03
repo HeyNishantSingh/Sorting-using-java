63deaea6537935007a147f7545b05dce971e477e
nishant <email.nishant.singh@gmail.com>
2020-07-03 21:32:20 +0530

    Selection Sort

---
+++ SelectionSort.java
@@ -0,0 +1,40 @@
+package com.company.Sorting;
+
+import java.util.Scanner;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+
+        int[] a = {5, 4, 3, 2, 1};
+        int n = a.length;
+
+        for (int i = n - 1; i > 0; i--) {
+            int largest = 0;
+            for (int j = 1; j <= i; j++) {
+                if (a[j] > a[largest]) {
+                    largest = j;
+                }
+                swap(a, largest, i);
+
+
+            }
+        }
+
+        //enhanced for loop to print the output
+        for (int i : a) {
+            System.out.print(i + " ");
+
+        }
+
+    }
+
+    public static void swap(int[] array, int i, int j) {
+        if (i == j) {
+            return;
+        } else {
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
+}
