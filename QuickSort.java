37c411ea3f9e4095dc7d501377fc0bcb9e5bb53a
nishant <email.nishant.singh@gmail.com>
2020-11-22 18:39:03 +0530

    QuickSort needs to be done right

--- QuickSort.java
+++ QuickSort.java
@@ -3,39 +3,42 @@ package com.company.sorting;
 public class QuickSort {
     public static void main(String args[]) {
         int[] inputArr = {20, 35, -15, 7, 55, 1, -22};
-        quickSort(inputArr, 0, inputArr.length);
+
+        //call quicksort
+        quickSort(inputArr, 0, inputArr.length - 1);
 
         for (int element : inputArr) {
             System.out.print(element + " ");
         }
     }
 
-    private static void quickSort(int[] a, int start, int end) {
-        if (end - start < 2)
-            return;
+    private static void quickSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int pivot = partition(arr, left, right);
 
-        int partionIndex = partition(a, start, end);
-        quickSort(a, start, partionIndex);
-        quickSort(a, partionIndex + 1, end);
+            quickSort(arr, left, pivot - 1);
+            quickSort(arr, pivot + 1, right);
+        }
     }
 
-    private static int partition(int[] a, int start, int end) {
-        int i = start, j = end;
-        int pivot = a[start];
+    private static int partition(int[] arr, int left, int right) {
+        int pivot = arr[right];
+        int i = (left - 1);
 
-        while (i < j) {
-            // alternating between right to left and left to right
-            while (i < j && a[--j] >= pivot) ;
-            if (i < j) {
-                a[j] = a[j];
-            }
+        for (int j = left; j < right; j++) {
+            if (arr[j] <= pivot) {
+                i++;
 
-            while (i < j && a[++i] <= pivot) ;
-            if (i < j) {
-                a[j] = a[i];
+                int swapTemp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = swapTemp;
             }
         }
-        a[i] = pivot;
-        return i;
+
+        int swapTemp = arr[i + 1];
+        arr[i + 1] = arr[right];
+        arr[right] = swapTemp;
+
+        return i + 1;
     }
 }
